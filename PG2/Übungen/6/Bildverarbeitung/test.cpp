80719ab37107970259644cbdfdf66d20c5a0410e
42p4r4gu2 <maxi.preiss55@gmail.com>
2024-04-29 16:01:15 +0200

    PG2 6 fertig

--- PG2/Übungen/6/Bildverarbeitung/test.cpp
+++ PG2/Übungen/6/Bildverarbeitung/test.cpp
@@ -1,7 +1,18 @@
 #include <png++/png.hpp>
 
 int main(){
-    png::image<png::rgb_pixel> image("apple.png");
-    image.write("output.png");
+    png::image<png::rgb_pixel> image("apple-94.png");
+
+    for(png::uint_32 y = 0; y < image.get_height(); ++y){
+        for(png::uint_32 x = 0; x < image.get_width(); ++x){
+            png::rgb_pixel pixel = image.get_pixel(x,y);
+            float luminance = 0.299 * pixel.red + 0.587 * pixel.green + 0.114 * pixel.blue;
+            pixel.red = luminance;
+            pixel.green = luminance;
+            pixel.blue = luminance;
+            image.set_pixel(x, y, pixel);
+        }
+    }
+    image.write("outputLumin.png");
 
 }
