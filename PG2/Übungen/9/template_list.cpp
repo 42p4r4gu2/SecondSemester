1a5a7eb60216539f0203334dfc7e882f062502b7
42p4r4gu2 <maxi.preiss55@gmail.com>
2024-07-02 22:38:00 +0200

    sugar honey ice and tea

--- PG2/Übungen/9/template_list.cpp
+++ PG2/Übungen/9/template_list.cpp
@@ -1,30 +1,80 @@
 #include <iostream>
+#include <string>
+
+using std::cout, std::endl, std::cerr;
 
 template <typename T>
+class dyn_array{
+private:
+    T* data;
+    int elements;
+public:
+    dyn_array() : data(nullptr), elements(0) {}
+    ~dyn_array(){
+        delete[] data;
+    }
+    dyn_array(dyn_array &&other) : data(std::move(other.data)), elements(len(data)){}
 
-class element{
-    private:
-    T val = 0;
-    element* next = nullptr;
+    template <typename T2>
+    friend std::ostream& operator<<(std::ostream &out, const dyn_array<T2> &list);
 
-    public:
-    element(){}
-    element(T val) : val(val) {}
-    element(T val, const element &next) : val(val), next(next) {}
-    element(const element &other) : val(other.val), next(other.next) {}
-    ~element() {}
+    dyn_array<T>& operator=(const dyn_array<T> &&src){ 
+    if(this != &src){
+        data = std::move(src.data);
+        elements = src.elements;
+        src.data = nullptr;
+    }
+    return *this;
+    }
+    void append(T elem){
+        T *new_data = new T[++elements];
+        for(int i = 0; i < elements-1; i++){
+            new_data[i] = data[i];
+        }
+        delete[] data;
+        data = new_data;
+        data[elements-1] = elem;
+    }
+    int getSize(){ 
+        return elements;
+    }
 
-    friend std::ostream operator<<(std::ostream &out, const element<T> &e);
+    T& operator[](int i){
+        return data[i];
+    }
 };
 
+
 template <typename T>
-std::ostream operator<<(std::ostream &out, const element<T> &e){
-    out << "[" << e.val << "|" << e.next << "]" << std::endl;
-    return out;
+std::ostream& operator<<(std::ostream &out, const dyn_array<T> &list){
+    for(int i = 0; i < list.elements; i++)
+        out << list.data[i] << " | ";
+    out << endl;
+    return out; 
 }
 
-int main(int argc, char ** argv){
-     element<int> first (1000);
-     element<char> second ('g');
-     std::cout << first << std::endl;
+template<typename T>
+dyn_array<T> returnMedian(const dyn_array<T> &arr){
+    int median = 0;
+    for(int i = 0; i < arr.getSize(); i++){
+        median += arr[i];
+    }
+    dyn_array<int> med;
+    med.append(median);
+    return med;
+}
+
+int main(int argc, char**argv){
+
+    dyn_array<int> myArr;
+
+    for(int i = 1; i < argc; i++){
+        myArr.append(atoi(argv[i]));
+        cout << myArr << endl;
+    }
+
+    dyn_array<int> myArr2;
+    myArr2 = returnMedian(myArr);
+    cout << myArr2 << endl;
+
 }
